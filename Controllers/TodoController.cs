e800ac750aa7bd19e634b3c2cd3ef399095fc102
wenz <wenz@spengergasse.at>
2024-11-29 11:34:33 +0100

    Update Docs

--- Controllers/TodoController.cs
+++ Controllers/TodoController.cs
@@ -21,14 +21,21 @@ namespace MyFirstWebApp.Controllers;
 //  PUT /api/todos/{id}       - Update a TodoItem
 //  DELETE /api/todos/{id}    - Delete a TodoItem
 
+// Status Codes are divided into 5 categories
+//  100-199 - Informational
+//  200-299 - Success
+//  300-399 - Redirection
+//  400-499 - Client Error
+//  500-599 - Server Error
+
 // Important Status Codes
-//  200 OK                    - The request was successful
-//  201 Created               - The request was successful and a new resource was created
-//  204 No Content            - The request was successful and there is no content to return
-//  400 Bad Request           - The request was invalid by the client
-//  401 Unauthorized          - The request requires authentication
-//  404 Not Found             - The requested resource was not found
-//  500 Internal Server Error - An unexpected error occurred
+//  200 OK                    - The request was successful, e.g. get all TodoItems
+//  201 Created               - The request was successful, a new resource was created, e.g. create a new TodoItem
+//  204 No Content            - The request was successful, there is no content to return, e.g. delete a TodoItem
+//  400 Bad Request           - The request was invalid, e.g. missing required fields
+//  401 Unauthorized          - The request requires authentication, e.g. wrong username or password
+//  404 Not Found             - The requested resource was not found, e.g. invalid id of the resource
+//  500 Internal Server Error - An unexpected error occurred, e.g. database connection failed, Server Unavailable, etc.
 
 
 [ApiController]
@@ -43,7 +50,7 @@ public class TodoController(TodoContext db) : ControllerBase
         // 1. Get all TodoItems from the database
         var todos = db.TodoItems.ToList();
         
-        // 2. Return 200 OK status code with the list of TodoItems
+        // 2. Return (200 OK) status code with the list of TodoItems
         return Ok(todos);
     }
     
@@ -57,11 +64,11 @@ public class TodoController(TodoContext db) : ControllerBase
         var todo = db.TodoItems.Find(id);
         if (todo == null)
         {
-            // 2. Return 404 Not Found status code if the TodoItem is not found
+            // 2. Return (404 Not Found) status code if the TodoItem is not found
             return NotFound(new { Message = $"TodoItem with Id {id} not found." });
         }
         
-        // 3. Return 200 OK status code if the TodoItem is found with the TodoItem
+        // 3. Return (200 OK) status code if the TodoItem is found with the TodoItem
         return Ok(todo);
     }
     
@@ -71,63 +78,61 @@ public class TodoController(TodoContext db) : ControllerBase
     [HttpPost]
     public IActionResult CreateTodo(AddTodoTaskCommand newTodo)
     {
-        // 1. Create a new TodoItem and add to the database
+        // 1. Create a new TodoItem
         var todoItem = new TodoItem(newTodo.Title, newTodo.IsDone, newTodo.DueAt, newTodo.Priority);
         
-        // 2. Add the TodoItem to the DBSet Database context and save the changes to the Database
+        // 2. Add the TodoItem and save DB changes
         db.TodoItems.Add(todoItem);
         db.SaveChanges();
         
-        // 3. Return a 201 Created status code with the new TodoItem
+        // 3. Return (201 Created) status code and the new TodoItem
         return CreatedAtAction(nameof(GetTodoById), new { id = todoItem.Id }, newTodo);
     }
 
     
     
     // HTTP PUT /api/todos/{id}
-    [HttpPut("{id}")]
+    [HttpPut("{id:int}")]
     public IActionResult UpdateTodo(int id, AddTodoTaskCommand updatedTodo)
     {
-        // 1. Find the TodoItem by its primary key form the DBSet Database context
+        // 1. Find the TodoItem by its primary key from the DB
         var todo = db.TodoItems.Find(id);
         if (todo == null)
         {
-            // 2. Return 404 Not Found status code if the TodoItem is not found
+            // 2. Return (404 Not Found) status code if the TodoItem is not found
             return NotFound(new { Message = $"TodoItem with Id {id} not found." });
         }
         
-        // 3. Update the existing TodoItem
+        // 3. Update the TodoItem and save DB changes
         todo.Title = updatedTodo.Title;
         todo.IsDone = updatedTodo.IsDone;
         todo.DueAt = updatedTodo.DueAt;
         todo.Priority = updatedTodo.Priority;
-        
-        // 4. Save the changes to the Database
         db.SaveChanges();
 
-        // 5. Return 200 OK status code with the updated/existing TodoItem
+        // 4. Return (200 OK) status code with the updated TodoItem
         return Ok(todo);
     }
 
     
     
     // DELETE /api/todos/{id}
-    [HttpDelete("{id}")]
+    [HttpDelete("{id:int}")]
     public IActionResult DeleteTodoById(int id)
     {
-        // 1. Find the TodoItem by its primary key form the DBSet Database context
+        // 1. Find the TodoItem by its primary key form the DB
         var todo = db.TodoItems.Find(id);
         if (todo == null)
         {
-            // 2. Return 404 Not Found status code if the TodoItem is not found
+            // 2. Return (404 Not Found) status code if the TodoItem is not found
             return NotFound(new { Message = $"TodoItem with Id {id} not found." });
         }
 
-        // 3. Remove the TodoItem from the database
+        // 3. Remove the TodoItem and save DB changes
         db.TodoItems.Remove(todo);
         db.SaveChanges();
         
-        // 4. Return 204 No Content status code
+        // 4. Return (204 No Content) status code
         return NoContent();
     }
 }
